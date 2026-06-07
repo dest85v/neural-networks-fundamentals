cf4e69f423aa28ab4062af86d6cd5f1de65903d3
dest85v <dest85v@gmail.com>
2026-06-07 13:31:15 +0300

    Shallow Feedforward NNs

--- .laborantum/src/datasets/mnist_simple.py
+++ .laborantum/src/datasets/mnist_simple.py
@@ -2,15 +2,22 @@ import torchvision.datasets
 
 class MNISTSimpleDataset:
     def __init__(self, train=True):
-        ...
         ## Load MNIST dataset here
         ## YOUR CODE HERE
+        dataset = torchvision.datasets.MNIST(
+            root='~/.cache/mnist',
+            train=train,
+            download=True
+        )
+        self.X = dataset.data.float()
+        self.y = dataset.targets.long()
 
 
     def __len__(self):
         res = 0
         ## Return number of items that is there in the dataset
         ## YOUR CODE HERE
+        res = len(self.X)
         return res
 
 
@@ -19,5 +26,9 @@ class MNISTSimpleDataset:
 
         ## Return a sample of the dataset that corresponds to the input index
         ## YOUR CODE HERE
+        image = self.X[index] / 255.0 * 2.0 - 1.0
+        label = self.y[index]
+        sample['image'] = image
+        sample['label'] = label
         
         return sample 
--- .laborantum/src/models/feedforward/simple_fcnn.py
+++ .laborantum/src/models/feedforward/simple_fcnn.py
@@ -6,15 +6,26 @@ class SimpleFCNN(torch.nn.Module):
             channels=None,
             n_classes=10,
             activation=torch.nn.ReLU):
-        ...
         ## YOUR CODE HERE
         # Define network modules in the constructor
+        super().__init__()
+        if channels is None:
+            channels = [784]
+        layer_sizes = [channels[0]] + list(channels[1:]) + [n_classes]
+        backbone_layers = []
+        for i in range(len(layer_sizes) - 2):
+            backbone_layers.append(torch.nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
+            backbone_layers.append(activation())
+        self.backbone = torch.nn.Sequential(*backbone_layers)
+        self.classifier = torch.nn.Linear(channels[-1], n_classes)
         
         
     def __forward_kernel(self, signal):
         signal = signal.reshape([signal.shape[0], -1])
         ## YOUR CODE HERE
         # Pass the signal through the modules in forward
+        signal = self.backbone(signal)
+        signal = self.classifier(signal)
         
         return signal
 
@@ -36,6 +47,7 @@ class SimpleFCNN(torch.nn.Module):
         signal = batch['signals']['output']
         
         ## YOUR CODE HERE
+        signal = torch.argmax(signal, dim=1)
         
         # Put the processed result into the batch
         batch['postprocessed'] = {'class': signal}
